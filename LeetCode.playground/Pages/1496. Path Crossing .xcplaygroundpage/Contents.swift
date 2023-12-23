9ed3723288d2553e85b6b2490355bf37fa32c04b
Islombek <whoisit864@gmail.com>
2023-12-23 18:09:22 +0300

    1496

---
+++ LeetCode.playground/Pages/1496. Path Crossing .xcplaygroundpage/Contents.swift
@@ -0,0 +1,49 @@
+import Foundation
+//submitted
+
+
+class Solution {
+    func isPathCrossing(_ path: String) -> Bool {
+        
+        // n - вверх
+        // e -вправо
+        // s - вниз
+        // w - влево
+        var pointLocation = [0, 0]
+        var arrPoints:[[Int]] = []
+        arrPoints.append(pointLocation)
+        for i in path {
+            switch i {
+            case "N": 
+                pointLocation[0] += 1
+                break
+            case "E":
+                pointLocation[1] += 1
+                break
+            case "S":
+                pointLocation[0] -= 1
+                break
+            case "W":
+                pointLocation[1] -= 1
+                break
+            
+            default:
+                continue
+            }
+            if arrPoints.contains(pointLocation) {
+                return true
+            } else {
+                arrPoints.append(pointLocation)
+            }
+            
+            
+        }
+        print(arrPoints)
+        return false
+    }
+}
+
+var s = Solution()
+var path = "NES"
+print(s.isPathCrossing(path))
+
