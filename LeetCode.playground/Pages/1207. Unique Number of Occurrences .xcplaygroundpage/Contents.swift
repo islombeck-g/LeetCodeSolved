9a8666ce26f0e376d2d9b64e3e766e6501ea9fcf
Islombek <whoisit864@gmail.com>
2024-01-17 14:51:45 +0300

    update

--- LeetCode.playground/Pages/1207. Unique Number of Occurrences .xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/1207. Unique Number of Occurrences .xcplaygroundpage/Contents.swift
@@ -1,25 +1,21 @@
 import Foundation
 //submitted at 25.12.2023
-
+//updated at 17.01.2024(daily)
 class Solution {
     func uniqueOccurrences(_ arr: [Int]) -> Bool {
-        
         
         var numbers: Dictionary<Int, Int> = [:]
         
         for i in arr {
-            if let val = numbers[i] {
-                numbers[i]! += 1
-                continue
-            }
-            numbers[i] = 1
+           numbers[i, default: 0] += 1
         }
-        var count:Set<Int> = []
-        for (key, value) in numbers {
-            count.insert(value)
+        
+        var defaultSet:Set<Int> = []
+        for (_, value) in numbers {
+            defaultSet.insert(value)
         }
        
-        return numbers.count == count.count
+        return numbers.count == defaultSet.count
     }
 }
 
